7288e43b1a777fbdb70c17f31e510e49bc0c253d
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2026-01-31 00:30:02 +0400

    hotfix: remove href prop from button

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,12 +3,11 @@ interface ButtonProps {
   label: string;
   width?: string;
   classname?: string;
-  href: string;
 }
-export default function Button({ label, width, classname, href }: ButtonProps) {
+export default function Button({ label, width, classname }: ButtonProps) {
   return (
     <button
-      className={`${href}${classname} px-6 py-2 bg-amber-800 cursor-pointer hover:bg-amber-900 transition-all duration-200 ${width}`}
+      className={`${classname} px-6 py-2 bg-amber-800 cursor-pointer hover:bg-amber-900 transition-all duration-200 ${width}`}
     >
       {label}
     </button>
