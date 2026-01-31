8c64b0f16c8202055a05438569097274e525ab5a
mjavanagoale-creator <mjavanagoale@gmail.com>
2026-01-31 17:31:04 +0400

    update-auth

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,8 +3,14 @@ import Link from "next/link";
 
 export default function SignIn() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-900">
-      <form className="w-95 bg-neutral-800 p-6 rounded-sm shadow-lg">
+    <div
+      className="min-h-screen w-screen 
+  flex items-center justify-center
+  bg-[url('/Fitrock-assets/imgs/sign-background2.png')]
+  bg-cover bg-center bg-no-repeat
+"
+    >
+      <form className="w-95 bg-neutral-800 p-6 rounded-2xl shadow-lg">
         <h1 className="text-3xl font-bold text-center text-white">Sign in</h1>
 
         <input
@@ -19,15 +25,15 @@ export default function SignIn() {
           className="w-full bg-transparent border-b border-neutral-500 py-2 mb-4 text-white placeholder-gray-400 outline-none focus:border-orange-500"
         />
 
-        <Button
-          classname="w-full font-medium text-white rounded-md"
-          label="Sign in"
-        />
+        <Button bgColor="orange" classname="w-full font-medium">
+          Sign in
+        </Button>
+
         <p className="text-white flex justify-between text-sm mt-3">
           Don&apos;t have account ?{" "}
           <Link
             href={"/sign-up"}
-            className="text-amber-600 cursor-pointer underline"
+            className="text-[#E47C48] cursor-pointer underline"
           >
             Sign up
           </Link>
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,8 +3,14 @@ import Link from "next/link";
 
 export default function SignUp() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-900">
-      <form className="w-95 bg-neutral-800 p-6 rounded-sm shadow-lg">
+    <div
+      className="min-h-screen w-screen 
+  flex items-center justify-center
+  bg-[url('/Fitrock-assets/imgs/sign-background2.png')]
+  bg-cover bg-center bg-no-repeat
+"
+    >
+      <form className="w-95 bg-neutral-800 p-6 rounded-2xl shadow-lg">
         <h1 className="text-3xl font-bold text-center text-white">Sign up</h1>
         <input
           type="text"
@@ -29,15 +35,14 @@ export default function SignUp() {
           className="w-full bg-transparent border-b border-neutral-500 py-2 mb-4 text-white placeholder-gray-400 outline-none focus:border-orange-500"
         />
 
-        <Button
-          classname="w-full font-medium text-white rounded-md"
-          label="Sign up"
-        />
+        <Button bgColor="orange" classname="w-full font-medium">
+          Sign up
+        </Button>
         <p className="text-white flex justify-between text-sm mt-3">
           already have an account ?{" "}
           <Link
             href={"/sign-in"}
-            className="text-amber-600 cursor-pointer underline"
+            className="text-[#E47C48] cursor-pointer underline"
           >
             Sign In{" "}
           </Link>{" "}
