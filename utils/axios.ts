8b7d16fe71f0a82a97d5634179c7ea1f11dbb25c
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2026-05-21 20:02:20 +0400

    change axios config

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+const isServer = typeof window === "undefined";
 const isProduction = process.env.NODE_ENV === "production";
 
+const baseURL = isServer
+  ? `${isProduction ? process.env.NEXT_PUBLIC_URL : "http://localhost:3000"}/api`
+  : "/api";
+
 const api = axios.create({
-  baseURL: isProduction ? "/api" : "http://localhost:3000/api",
+  baseURL,
   withCredentials: true,
 });
 
